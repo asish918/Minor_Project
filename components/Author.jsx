aedef952b5a0c3a47204a80ea29b2e1f013ed765
Asish Mahapatra <asishmahapatra918@gmail.com>
2023-02-19 22:58:08 +0530

    Added and Styled the Author component

--- components/Author.jsx
+++ components/Author.jsx
@@ -1,7 +1,20 @@
-const Author = () => {
+import Image from "next/image"
+
+const Author = ({ author }) => {
     return (
-        <div>
-            Author
+        <div className="text-center mt-20 mb-8 p-12 relative rounded-lg bg-black bg-opacity-20 w-full">
+            <div className="absolute left-[45%] -top-14">
+                <Image
+                    alt="authorImage"
+                    height='100'
+                    width='100'
+                    className="align-middle rounded-full h-24 w-24 object-cover"
+                    src={author.photo.url}
+                />
+            </div>
+
+            <h3 className="text-white my-4 text-xl font-bold">{author.name}</h3>
+            <p className="text-white text-lg">{author.bio}</p>
         </div>
     )
 }
