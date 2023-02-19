d5185b2721f8cbb69563f9dd15a51fc8a09de850
Asish Mahapatra <asishmahapatra918@gmail.com>
2023-02-19 18:45:32 +0530

    Added and Styled the PostCard component

--- components/Header.jsx
+++ components/Header.jsx
@@ -13,7 +13,7 @@ const Header = () => {
                 <div className='md:float-left block'>
                     <Link href="/">
                         <span className='cursor-pointer font-bold text-4xl text-white'>
-                            Suffermen CMS
+                            Marvel CMS
                         </span>
                     </Link>
                 </div>
